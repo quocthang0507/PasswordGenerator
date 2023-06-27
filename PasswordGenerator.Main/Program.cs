8c18f446b85d76df32c5a6f72cc1c53c87459770
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2023-06-27 23:45:19 +0700

    Update program.cs

--- PasswordGenerator.Main/Program.cs
+++ PasswordGenerator.Main/Program.cs
@@ -1,4 +1,30 @@
 ﻿using PasswordGenerator;
+using System.Text;
 
-var password = new Password(10);
-Console.WriteLine(password.Next());
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        string[] arguments = Environment.GetCommandLineArgs();
+
+        int length = 1;
+        var pwd = new Password(includeLowercase: true, includeUppercase: true, includeNumeric: true, includeSpecial: false, passwordLength: 10);
+
+        if (arguments.Length == 2)
+            if (int.TryParse(arguments[1], out length))
+            {
+                string[] passwords = pwd.NextGroup(length).ToArray();
+                Console.WriteLine(string.Join('\n', passwords));
+            }
+            else
+            {
+                string password = pwd.Next();
+                Console.WriteLine(password);
+            }
+        else
+        {
+            Console.WriteLine("Wrong number of arguments or invalid arguments");
+        }
+    }
+}
